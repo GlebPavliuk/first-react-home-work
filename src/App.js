2f1351a37fda2fc7d5ff58719be32f3604074e47
Glib Pavliuk <gleb_ua@ukr.net>
2022-03-08 12:39:47 +0200

    Feature: create 3 blocks, header, main container and footer

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="my-header">My Header</header>
+      <div className="my-container">Слава Україні!</div>
+      <footer className="my-footer">My Footer</footer>
+    </div>
+  );
+}
+
+export default App;
